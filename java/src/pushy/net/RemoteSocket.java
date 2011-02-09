6eaffb1bf76bfa7436e00d541875c80982ee0a98
Andrew Wilkins <axwalk@gmail.com>
2011-02-09 22:27:08 +0800

    Fixing copyright dates

--- java/src/pushy/net/RemoteSocket.java
+++ java/src/pushy/net/RemoteSocket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Andrew Wilkins <axwalk@gmail.com>
+ * Copyright (c) 2009, 2011 Andrew Wilkins <axwalk@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
