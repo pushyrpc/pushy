2e4720fdf087a430e024812626dff1d6ac930eb3
Andrew Wilkins <axwalk@gmail.com>
2013-07-21 22:25:44 +0800

    Release 0.5.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ dependencies = [
 
 setup(
     name="pushy",
-    version="0.5.1",
+    version="0.5.2",
     description = description,
     long_description = long_description,
     classifiers = classifiers,
